aefccc809123acaf54f2cabfca0bbb5d37d9e466
Alexander-Bokedal <alexander.bokedal@gmail.com>
2025-03-28 11:05:37 +0100

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Button } from './components/button'
 import { Footer } from './components/footer'
+import { Header } from './components/header'
 function App() {
 
   return (
     <>
+      <Header />
       <Button />
       <Footer />
 
